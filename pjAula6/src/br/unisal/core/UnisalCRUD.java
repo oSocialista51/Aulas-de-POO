36ec7a962bb3b8fbf60625a70306525de61b1e92
oSocialista51 <159207824+oSocialista51@users.noreply.github.com>
2024-03-19 21:12:05 -0300

    Add files via upload

---
+++ pjAula6/src/br/unisal/core/UnisalCRUD.java
@@ -0,0 +1,15 @@
+package br.unisal.core;
+
+public abstract class UnisalCRUD {
+	//Métodos abstratos
+	public abstract void insere(Object obj);
+	public abstract Object pesquisa(int id);
+	public abstract void atualiza(Object obj);
+	public abstract boolean remove(int id);
+	
+	public String toString() {
+		return "Tipo " + this.getClass().getName() + " HashCode " + this.hashCode();
+				
+	}
+	
+}	
---
+++ pjAula6/src/br/unisal/modelagem/frmFornecedor.java
@@ -0,0 +1,36 @@
+package br.unisal.modelagem;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.swing.JFrame;
+import java.util.List;
+
+import br.unisal.core.Grid;
+
+public class frmFornecedor extends JFrame implements ActionListener, Grid {
+
+	@Override
+	public List<Object> getDataSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean atualiza() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void limpa() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
